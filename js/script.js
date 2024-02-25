8bc4305c522b66619eb5f24358c06b2ece6a2e7f
sjpark07 <159090533+sjpark07@users.noreply.github.com>
2024-02-25 22:58:22 +0900

    240225

--- js/script.js
+++ js/script.js
@@ -592,9 +592,9 @@ function getListItem(index, places) {
 // 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
 function addMarker(position, idx, title) {
   var imageSrc = '../img/marker.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
-    imageSize = new kakao.maps.Size(36, 37), // 마커 이미지의 크기
+    imageSize = new kakao.maps.Size(36, 54), // 마커 이미지의 크기
     imgOptions = {
-      spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+      spriteSize: new kakao.maps.Size(36, 975), // 스프라이트 이미지의 크기
       spriteOrigin: new kakao.maps.Point(0, (idx * 46) + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
       offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
     },
